e0223b3d4822aecd6a60abd785d2c979a02c0c30
syrenko <ogarkova@profi.travel>
2025-06-30 13:21:31 +0500

    Finished practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { Product } from "./components/Product";
-import { Loader } from "./components/Loader";
-import { ErrorMessage } from "./components/ErrorMessage";
-import { useProducts } from "./hooks/products";
-import { Modal } from "./components/Modal";
-import { CreateProduct } from "./components/CreateProduct";
-import { useState } from "react";
-import { IProduct } from "./models";
+import { Route, Routes } from "react-router-dom";
+import { ProductPage } from "./pages/ProductsPage";
+import { AboutPage } from "./pages/AboutPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
-  const { loading, error, products, addProduct } = useProducts();
-  const [modal, setModal] = useState(true);
-
-  const createHandler = (product: IProduct) => {
-    setModal(false);
-    addProduct(product);
-  }
-
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products?.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
-
-      {modal && (
-        <Modal title="Create new product">
-          <CreateProduct onCreate={createHandler}/>
-        </Modal>
-      )}
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 }
 
